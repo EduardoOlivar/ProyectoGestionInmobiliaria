398c9b35dbd66592705383f4d6484eb9eafcc30e
EduardoOlivar <eduardo.olivar.aros@gmail.com>
2021-08-28 19:58:23 -0400

    Entrega 1

--- src/main/java/com/mycompany/proyectoinmobiliaria/GestionInmobiliaria.java
+++ src/main/java/com/mycompany/proyectoinmobiliaria/GestionInmobiliaria.java
@@ -14,46 +14,58 @@ public class GestionInmobiliaria {
 
     public static void main(String[] args) throws IOException 
     {
-        
+        /*Aqui usamos el metodo para pedir datos por consola*/
         BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Ingrese la cantidad de Edificios que quiera agregar");
         int cantidadEdificio = Integer.parseInt(leer.readLine());
-        
-        Edificio[] edificios = new Edificio[cantidadEdificio];//String nombre, String localidad, String direccion, String arquitecto
+        /*instanciador de un arreglo de edificios con la cantidad que el usuario quiera*/
+        Edificio[] edificios = new Edificio[cantidadEdificio];
+        /*metodo para llenar el arreglo de edificios*/
         llenadoEdificiosPorConsola(edificios);
         
         System.out.println("Ingrese la cantidad de Pisos que quiera agregar");
         int cantidadPiso = Integer.parseInt(leer.readLine());
+        /* instanciador de un arreglo de pisos con la cantidad que el usuario quiera*/
         Piso[] piso = new Piso[cantidadPiso];
+        /*metodo para llenar el arreglo de pisos*/
         llenadoPiso(piso);
         
+        /*metodos para mostrar por pantalla los objetos que el usuario incorpore
+          en un arreglo de tipo edificio y en un arreglo de tipo piso
+        */
         mostrarEdificios(edificios);
         mostrarPiso(piso);
         
-        
+        /*Parametros para objetos iniciales de clase Edificio*/
         Edificio objEdificio1 = new Edificio("10 norte","10 norte 655","Viña del mar","Quiroz y Puelma Arquitectos");
         Edificio objEdificio2 = new Edificio("15 norte","15 norte 242","Viña del mar","Mario Castañeda");
         Edificio objEdificio3 = new Edificio("Viana Miramar","Viana 161","Viña del mar","Roberto Rau");
+        /*Mostrar por pantalla los objetos iniciales*/
         objEdificio1.mostrarEdificio();
         objEdificio2.mostrarEdificio();
         objEdificio3.mostrarEdificio();
+        
+        /*Parametros para objetos iniciales de clase Piso*/
         Piso objPiso1 = new Piso(1,4);
         Piso objPiso2 = new Piso(2,4);
         Piso objPiso3 = new Piso(3,4);
         Piso objPiso4 = new Piso(4,4);
+        /*Mostrar por pantalla los objetos iniciales*/
         objPiso1.mostrarPiso();
         objPiso2.mostrarPiso();
         objPiso3.mostrarPiso();
         objPiso4.mostrarPiso();
-        
+        /*Parametros para objetos iniciales de clase Departamento*/
         Departamento objDepartamento1 = new Departamento ("22A","3000UF","Poniente",2,3,67);
         Departamento objDepartamento2 = new Departamento("51A","3500UF","oriente",2,3,109);
+        
+        /*Mostrar por pantalla los objetos iniciales*/
         objDepartamento1.mostrarDpto();
         objDepartamento2.mostrarDpto();
         
     }
     
-    // metodo para agregar lista de Edificios.
+    /* metodo para agregar Edificios por consola.*/
     public static void llenadoEdificiosPorConsola(Edificio[] edificios) throws IOException 
     {
         
@@ -71,6 +83,7 @@ public class GestionInmobiliaria {
             edificios[i].setArquitecto(leer.readLine());            
         }           
     }
+    /* metodo para llamar a mostrarEdificio*/
     public static void mostrarEdificios(Edificio[] edificios)
     {
         for(int i = 0 ; i < edificios.length;i++)
@@ -78,6 +91,8 @@ public class GestionInmobiliaria {
             edificios[i].mostrarEdificio();
         }  
     }
+    
+    /*Metodo para llenar piso por consola*/
     public static void llenadoPiso(Piso[] piso) throws IOException
     {
         BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
@@ -93,6 +108,7 @@ public class GestionInmobiliaria {
         }       
         
     }
+    /* metodo para llamar a mostrarPiso */
    public static void mostrarPiso(Piso[] piso) 
    {
       for(int i = 0 ; i < piso.length ; i++)
@@ -105,7 +121,7 @@ public class GestionInmobiliaria {
    
     
     
-    //metodos para mas la proxima entrega para las anidaciones
+    //metodos para la proxima entrega en donde se incorporen anidaciones, no esta bien escrito e instanciado pero ahi se esta estudiando
     /*public static void llenadoPisos(Edificio edificios) throws IOException{
         
         
